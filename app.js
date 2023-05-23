2824a2c3b768e63bf23d033971c3dfcc5bbaf988
Kim Joohwan <kim.joohwan58@gmail.com>
2023-05-23 17:56:39 +0900

    rock paper scissors game added in console

---
+++ app.js
@@ -0,0 +1,47 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random()*3) + 1
+    switch (choice) {
+        case 1:
+            return "rock"
+            break;
+        case 2:
+            return "scissors"
+        case 3:
+            return "paper"
+        default:
+            break;
+    }
+}
+function playRound(player, computer) {
+    switch (player+computer) {
+        case "rockscissors":
+            return "You win! Rock beats scissors"
+            break;
+        case "rockpaper":
+            return "You lose. Paper beats rock"
+            break;
+        case "paperscissors":
+            return "You lose. Scissors beat paper"
+            break;
+        case "paperrock":
+            return "You win! Paper Beats rock"
+            break;
+        case "scissorpapper":
+            return "You win!, Scissors beat paper"
+            break;
+        case "scissorrock":
+            return "You lose. Rock beats scissors"
+            break;
+        default:
+            return "It's a draw!"
+            break;
+    }
+}
+function game() {
+    let player
+    for (i = 0; i < 5; i++) {
+        player = prompt("Play rps, enter option")
+        console.log(playRound(player, getComputerChoice()))
+    }
+}
+game()
